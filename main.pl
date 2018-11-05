bb0b0e55f986c5aeac880d31655812f90633819b
Rami <ramimatloob@gmail.com>
2018-11-04 19:49:26 -0500

    comments + formatting

--- main.pl
+++ main.pl
@@ -1,45 +1,50 @@
+%% Usage: for answers to q1-q10 query main
+%%---------------------
+
+%% input file containing train(X,Y,Z) predicates
 :-include('tests/test1.pl').
 
+%% q1: The distance of the route A-B-C.
 q1(O):-
 	train(a,b,P1),
 	train(b,c,P2),
     O is P1+P2.
-
 q1(O):-
 	O = "NO SUCH ROUTE".
-	
+
+%% The distance of the route A-D.
 q2(O):-
-    train(a,d,P1),
-    O is P1.
+    train(a,d,O).
 q2(O):-
 	O = "NO SUCH ROUTE".
 
+%% The distance of the route A-D-C.
 q3(O):-
     train(a,d,P1),
     train(d,c,P2),
     O is P1+P2.
-
 q3(O):-
 	O = "NO SUCH ROUTE".
 
+%% The distance of the route A-E-B-C-D.
 q4(O):-
     train(a,e,P1),
     train(e,b,P2),
     train(b,c,P3),
     train(c,d,P4),
     O is P1+P2+P3+P4.
-
 q4(O):-
 	O = "NO SUCH ROUTE".
-	
+
+%% The distance of the route A-E-D.	
 q5(O):-
     train(a,e,P1),
     train(e,d,P2),
     O is P1+P2.
-
 q5(O):-
 	O = "NO SUCH ROUTE".
-	
+
+%% connected6 is true if a train can go from X to Y in maximum Z stops 
 connected6(X,Y,Z):-
     train(X,Y,_),
     Z > 0.
@@ -50,34 +55,33 @@ connected6(X,Y,Z):-
 	Temp is Z-1,
     connected6(A,Y,Temp).
 
+%% The number of trips starting at C and ending at C with a maximum of 3 stops.
 q6(O):-
-	findall(_, connected6(c,c,3), X),
+	findall(_, connected6(c,c,3), X), %% find all the answers returned by query connected6(c,c,3)
 	length(X,O).
-	
 q6(O):-
 	O = "Invalid Input".
-	
+
+%% connected7 is true if a train can go from X to Y in exctly Z stops 
 connected7(X,Y,Z):-
     train(X,Y,_),
     Z is 1.
-
 connected7(X,Y,Z):-
 	Z>0,
     train(X,A,_),
 	Temp is Z-1,
     connected7(A,Y,Temp).
 
+%% The number of trips starting at A and ending at C with exactly 4 stops
 q7(O):-
-	findall(_, connected7(a,c,4), X),
+	findall(_, connected7(a,c,4), X), %% find all the answers returned by query connected7(a,c,4)
 	length(X,O).
-	
 q7(O):-
 	O = "Invalid Input".
 
-	
+%% distance is used to calculate Z which is the total distance between X and Y
 distance(X,Y,Z,_):-
 	train(X,Y,Z).
-
 distance(X,Y,Z,Pcurrent):-
 	train(X,A,Temp),
 	not(member(A,Pcurrent)),
@@ -85,48 +89,45 @@ distance(X,Y,Z,Pcurrent):-
 	distance(A,Y,Temp2,P),
 	Z is Temp+Temp2.
 
+%% list_min finds the minimum value in a list
 list_min([L|Ls], Min) :-
     list_min(Ls, L, Min).
-
 list_min([], Min, Min).
 list_min([L|Ls], Min0, Min) :-
     Min1 is min(L, Min0),
     list_min(Ls, Min1, Min).
 	
+%% The length of the shortest route (in terms of distance to travel) from A to C.
 q8(O):-
-	findall(Temp, distance(a,c,Temp,[]), X),
-	list_min(X,T),
-	O is T.	
-	
+	findall(Temp, distance(a,c,Temp,[]), X), %% find all the answers returned by query distance(a,c,Temp,[])
+	list_min(X,O).
 q8(O):-
 	O = "Invalid Input".
-	
+
+%% The length of the shortest route (in terms of distance to travel) from B to B.
 q9(O):-
-	findall(Temp, distance(b,b,Temp,[]), X),
-	list_min(X,T),
-	O is T.	
-	
+	findall(Temp, distance(b,b,Temp,[]), X), %% find all the answers returned by query distance(b,b,Temp,[])
+	list_min(X,O).	
 q9(O):-
 	O = "Invalid Input".
-	
+
+%% connected10 is true if a train can go from X to Y with a distance less than Z
 connected10(X,Y,Z):-
     train(X,Y,Temp),
     Z > Temp.
-
 connected10(X,Y,Z):-
     train(X,A,Temp2),
 	Temp is Z-Temp2,
 	Temp>0,
     connected10(A,Y,Temp).	
-	
+
+%% The number of different routes from C to C with a distance of less than 30. 
 q10(O):-
-	findall(_, connected10(c,c,30), X),
+	findall(_, connected10(c,c,30), X), %% find all the answers returned by query connected10(c,c,30)
 	length(X,O).
-	
 q10(O):-
 	O = "Invalid Input".
 	
-
 main:-
 	q1(Q1),
 	q2(Q2),
